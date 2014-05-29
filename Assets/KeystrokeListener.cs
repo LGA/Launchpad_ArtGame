d868c28f41064ed265c9e53ed0571afc1fcf87b7
LGA <l.gartlehner@gmx.at>
2014-05-29 22:53:40 +0200

    Initial Commit - Project Stub

---
+++ Assets/KeystrokeListener.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeystrokeListener : MonoBehaviour {
+
+
+	public GameObject simpleSphere;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// Keycodes http://docs.unity3d.com/ScriptReference/KeyCode.html
+
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			simpleSphere.rigidbody.AddForce (0, 0, -10);
+			Debug.Log("Key: 2");
+		}
+
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			simpleSphere.rigidbody.AddForce (10, 0, 0);
+			Debug.Log("Key: Q");
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			simpleSphere.rigidbody.AddForce (0, 0, 10);
+			Debug.Log("Key: S");
+		}
+
+		if (Input.GetKeyDown (KeyCode.E)) {
+			simpleSphere.rigidbody.AddForce (-10, 0, 0);
+			Debug.Log("Key: E");
+		}
+
+	}
+}
